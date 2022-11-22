1c7ca1727abcdaa0dec886d4d27225f016942024
GilbyKoloay <lolwtf10001@gmail.com>
2022-11-23 01:21:06 +0800

    add index.html into App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { logoBot } from './icons';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card d-flex flex-column vh-100 overflow-hidden">
+				<div className="card-body" style={{overflowY:"scroll"}} id="content-chat-feed">
+          <div className="containerbot">
+          <img src={logoBot} alt="Avatar" style={{width:"100%"}} />
+          <div className="row">
+            <div className='col-sm-8 pt-4'>Hi, selamat datang :)</div>
+            <div className='col-sm-4 pt-4'><span className='time-right'>98.99%<br />11:00 PM</span></div>
+          </div>
+        </div>
+				</div> 
+				<div className="card-footer"> 
+					<div className="input-group">
+						<input type="text" className="form-control" id="input-chat" />
+						<div className="input-group-append">
+							<button className="btn btn-primary" type="button">Send</button>
+						</div>
+					</div>
+				</div>
+			</div>
     </div>
   );
 }
-
-export default App;
